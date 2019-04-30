e4e0b464ce24bffe864f2ba17c8b9d81063af5cf
Bjorn Boe <bboe@pivotal.io>
2019-04-30 11:16:31 -0400

    New test framework

--- tcp-test-apps/tcp-client/src/main/java/io/pivotal/bboe/tcpclient/TestController.java
+++ tcp-test-apps/tcp-client/src/main/java/io/pivotal/bboe/tcpclient/TestController.java
@@ -7,8 +7,8 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.BufferedReader;
@@ -53,44 +53,89 @@ public class TestController {
 
     @RequestMapping("/")
     public String usageString() {
-        return "Usage: <test-app-url>/runtest/<iterations>";
+        return "Usage: curl <test-app-url>/runtest \\" +
+                "\n-d iterations=1000 \\" +
+                "\n-d payloadsize=1 \\" +
+                "\n-d bigtest=false \\" +
+                "\n-d mintestruns=1";
     }
 
-    @RequestMapping("/runtest/{iterations}")
-    public String runTest(@PathVariable("iterations") int iterations) {
-        logger.info("Starting test with " + iterations + " iterations");
+    @RequestMapping("/runtest")
+    public String runTest(@RequestParam(value="iterations", defaultValue="100") int iterations,
+                          @RequestParam(value="payloadsize", defaultValue="1") int payloadsize,
+                          @RequestParam(value="bigtest", defaultValue="false") boolean bigtest,
+                          @RequestParam(value="mintestruns", defaultValue="1") int mintestruns) {
+        logger.info("Run test. Iterations: " + iterations + " Payload size: " + payloadsize + " Big test: " + bigtest + " Minimum test runs: " + mintestruns);
 
         String result = null;
+        String testString = generateTestString(payloadsize);
 
-        try{
-            logger.info("Connecting to " + serverAddress + " on port " + serverPort);
-            Socket clientSocket = getNewClientSocket();
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        try {
+            double bestResult = Double.MAX_VALUE;
+            int testCounter = 0;
+            while(true) {
+                logger.info("Run test #" + testCounter);
+                double newResult = runSingleTest(iterations, testString);
+                logger.info("Test time: " + String.format("%1$,.2f", newResult) + "ms");
 
-            SimpleTimer t = new SimpleTimer();
-            logger.info("Starting");
-            for(int i = 0; i < iterations; i++) {
-                doRunTest(out, in);
+                testCounter++;
+                if(testCounter < mintestruns) {
+                    if(newResult < bestResult) {
+                        bestResult = newResult;
+                    }
+                } else {
+                    if(newResult < bestResult) {
+                        bestResult = newResult;
+                        if(!bigtest) {
+                            break;
+                        }
+                    } else {
+                        break;
+                    }
+                }
             }
-            result = generateResultString(iterations, t);
-            out.close();
-            in.close();
-            clientSocket.close();
-            logger.info(result);
+
+            result = generateResultString(iterations, bestResult);
         } catch (Exception e) {
             result = "Test failed - " + e.getMessage();
             e.printStackTrace();
         }
 
+        logger.info(result);
         return result;
     }
 
-    private void doRunTest(PrintWriter out, BufferedReader in) throws IOException {
+    private String generateTestString(int length) {
+        String result = "";
+        for(int i = 0; i < length; i++) {
+            result = result + "A";
+        }
+        return result;
+    }
+
+    public double runSingleTest(int iterations, String testString) throws IOException {
+        logger.info("Connecting to " + serverAddress + " on port " + serverPort);
+        Socket clientSocket = getNewClientSocket();
+        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+        BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        SimpleTimer t = new SimpleTimer();
+        logger.info("Starting");
+        for(int i = 0; i < iterations; i++) {
+            doRunTest(out, in, testString);
+        }
+        double result = t.getTime();
+        out.close();
+        in.close();
+        clientSocket.close();
+        return result;
+    }
+
+    private void doRunTest(PrintWriter out, BufferedReader in, String testString) throws IOException {
         logger.debug("Run...");
         long startTime = System.nanoTime();
         try {
-            out.println("Ping");
+            out.println(testString);
             out.flush();
             in.readLine();
         } finally {
@@ -99,16 +144,12 @@ public class TestController {
         }
     }
 
-
-    private String generateResultString(int iterations, SimpleTimer timer) {
-        double time = timer.getTime();
+    private String generateResultString(int iterations, double time) {
         String iterationsString = String.format("%,d", iterations);
         String totalTimeString = String.format("%1$,.2f", time);
         String avgLatencyString = String.format("%1$,.4f", time / iterations);
         String txPerSecondString = String.format("%,d", (int)(iterations / time * 1000));
 
-
         return "Iterations: " + iterationsString + ", Total Time: " + totalTimeString + "ms, Avg latency: " + avgLatencyString + "ms, TX pr second: " + txPerSecondString;
     }
-
 }
